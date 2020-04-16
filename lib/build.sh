20c7bd9b7926290e270213f2ae907d50563823bd
Lillian Zhang <zhang.l@salesforce.com>
2020-04-15 17:27:16 -0700

    detect outDir in tsconfig.json

--- lib/build.sh
+++ lib/build.sh
@@ -5,3 +5,11 @@ bp_dir=$(cd "$(dirname "$BASH_SOURCE")" || exit; cd ..; pwd)
 
 # shellcheck source=/dev/null
 source "$bp_dir/lib/utils/log.sh"
+source "$bp_dir/lib/utils/json.sh"
+
+detect_out_dir() {
+  local build_dir=$1
+  out_dir=$(json_get_key "$build_dir/tsconfig.json" ".compilerOptions.outDir")
+
+  [[ -f "$build_dir/$outDir" ]]
+}
--- shpec/build_shpec.sh
+++ shpec/build_shpec.sh
@@ -5,3 +5,30 @@ set -o pipefail
 
 source "./lib/utils/log.sh"
 source "./lib/build.sh"
+
+create_temp_project_dir() {
+  mktemp -dt project_shpec_XXXXX
+}
+
+describe "lib/build.sh"
+  describe "detect_out_dir"
+    it "exits with 1 if there is no outDir directory"
+      project_dir=$(create_temp_project_dir)
+
+      set +e
+      detect_out_dir "$project_dir"
+      loc_var=$?
+      set -e
+
+      assert equal "$loc_var" 1
+    end
+
+    it "exits with 0 if there is outDir directory"
+      project_dir=$(create_temp_project_dir)
+      touch "$project_dir/dist"
+
+      detect_out_dir "$project_dir"
+
+      assert equal "$?" 0
+    end
+  end
